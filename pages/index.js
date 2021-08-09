c3430b3e06e0fe67e96de45a03794064d4e88276
EswaraPrakash Vaithiyanathan <v_eswara@live.com>
2021-08-09 06:21:29 +0530

    Fix Build issue dueo to utlities

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
-import { degToCompass, convertTime, ctoF, mpsToMph, kmToM, timeToAMPM, isPM } from './utils.js'; //utlity file.
+import { degToCompass, convertTime, ctoF, mpsToMph, kmToM, timeToAMPM, isPM } from '../utilities/utils.js'; //utlity file.
 
 export default function Home() {
   const [input, setInput] = useState("Chennai"); // default city
@@ -30,7 +30,7 @@ export default function Home() {
 
   useEffect(() => {
     getData();
-  }, []);
+  });
 
   
 
